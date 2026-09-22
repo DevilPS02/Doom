d297d6cf0f67ef085b6693e96e101ff0e0d3ee54
DevilPS02 <jsgosanchez345@gmail.com>
2026-09-22 12:48:25 -0500

    avance de armas parte 2
    So de armas, manager y gun controller

---
+++ My project/Assets/_Scripts/Enemy/Enemy.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int health = 10;
+
+    public GameObject blood;
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyDead();
+    }
+
+    public void Damage(int damage, Quaternion rot)
+    {
+        health -= damage;
+        GameObject bloodInstance = Instantiate(blood, transform.position, rot);
+        Destroy(bloodInstance, 2);
+    }
+
+    public void EnemyDead()
+    {
+        if(health<= 0)
+        {
+            EnemyManager.Instance.RemoveEnemy(this);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ My project/Assets/_Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager Instance;
+
+    public List<Enemy> enemiesInRange = new List<Enemy>();
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void AddEnemy(Enemy enemy)
+    {
+        enemiesInRange.Add(enemy);
+    }
+
+    public void RemoveEnemy(Enemy enemy)
+    {
+        enemiesInRange.Remove(enemy);
+    }
+}
